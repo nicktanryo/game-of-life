df8f5ab58139161ab4d7298aaf3adb874b7ba587
Nicholas Tanryo <nick37tanryo@gmail.com>
2020-07-20 21:07:32 +0700

    chage structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import React from 'react'
 import "./App.css"
 
 import GameOfLife from './components/GameOfLife'
-import Title from './components/Title'
 
 export default function App(): JSX.Element {
     return (
         <div className="App">
-            <Title />
             <GameOfLife />
         </div>
     )
