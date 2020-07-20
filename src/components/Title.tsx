b9e6c6271691f5c9c36ad3ea37592639c2a4e47d
Nicholas Tanryo <nick37tanryo@gmail.com>
2020-07-20 21:08:18 +0700

    change to appbar and add buttons

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,13 +1,57 @@
 import React from 'react'
-import { AppBar, Toolbar, Typography } from '@material-ui/core'
+import { AppBar, Toolbar, Typography, makeStyles, Grid, Button } from '@material-ui/core'
 
-export default function Title(): JSX.Element {
+interface Props {
+    running: boolean
+    handleSimulation: () => void
+    generatePopulation: () => void
+}
+
+const useStyles = makeStyles(() => ({
+    appbar: {
+        marginBottom: "30px"
+    },
+    title: {
+        flex: 1,
+        display: "inline",
+        marginLeft: "10px"
+    },
+    button: {
+        margin: "auto 10px auto auto"
+    }
+}))
+
+export default function Title({ running, handleSimulation, generatePopulation }: Props): JSX.Element {
+    const classes = useStyles()
     return (
-        <AppBar position="static">
+        <AppBar position="static" className={classes.appbar}>
             <Toolbar>
-                <Typography variant="h6">
-                    GAME OF LIFE
-                </Typography>
+                <Grid container justify="space-between">
+                    <Grid item>
+                        <Typography variant="h6" className={classes.title}>
+                            GAME OF LIFE
+                        </Typography>
+                    </Grid>
+
+                    <Grid item>
+                        <Button
+                            variant="outlined"
+                            className={classes.button}
+                            color="inherit"
+                            onClick={generatePopulation}
+                        >
+                            GENERATE CELLS
+                        </Button>
+                        <Button
+                            color={running ? "secondary" : "inherit"}
+                            onClick={handleSimulation}
+                            variant="outlined"
+                            className={classes.button}
+                        >
+                            {running ? "STOP" : "START"}
+                        </Button>
+                    </Grid>
+                </Grid>
             </Toolbar>
         </AppBar>
     )
