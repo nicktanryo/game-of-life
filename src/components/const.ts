80ffdc7944301a60b657f9ab82663d5bf7e822e5
Nicholas Tanryo <nick37tanryo@gmail.com>
2020-07-20 21:07:51 +0700

    add consts

--- src/components/const.ts
+++ src/components/const.ts
@@ -1,6 +1,6 @@
-const GRID_ROW: number = 20;
-const GRID_COLUMN: number = 40;
-const CELL_SIZE: number = 30
+const GRID_ROW: number = 25;
+const GRID_COLUMN: number = 50;
+const CELL_SIZE: number = 25
 const BORDER_SIZE = 1
 
 const OPERATION: number[][] = [
@@ -14,4 +14,17 @@ const OPERATION: number[][] = [
     [-1, 0]
 ];
 
-export { GRID_ROW, GRID_COLUMN, CELL_SIZE, BORDER_SIZE, OPERATION };
+const DELAY: number = 100
+
+const GENERATE_POPULATION = (grid: number[][]): number[][] => {
+    for (let i = 0; i < GRID_ROW; i++) {
+        for (let j = 0; j < GRID_COLUMN; j++) {
+            const chance: number = Math.random()
+            if (chance > 0.5) grid[i][j] = 1
+            else grid[i][j] = 0
+        }
+    }
+    return grid
+}
+
+export { GRID_ROW, GRID_COLUMN, CELL_SIZE, BORDER_SIZE, OPERATION, DELAY, GENERATE_POPULATION };
