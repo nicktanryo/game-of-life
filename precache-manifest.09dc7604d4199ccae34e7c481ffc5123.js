8c23fa2db3100c6662444cc9cc6cfc084f0eeda1
Nicholas Tanryo <nick37tanryo@gmail.com>
2020-07-26 17:50:35 +0700

    Updates

---
+++ precache-manifest.09dc7604d4199ccae34e7c481ffc5123.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "914bd03e39eb3294a72a3ff5db93ea75",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "74ad6fd074b5529f0e33",
+    "url": "/game-of-life/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "2c1901916ea2dbc9e3d8",
+    "url": "/game-of-life/static/js/2.cb5f82fa.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/game-of-life/static/js/2.cb5f82fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74ad6fd074b5529f0e33",
+    "url": "/game-of-life/static/js/main.dbbfe475.chunk.js"
+  },
+  {
+    "revision": "a6893dc5f1659096e143",
+    "url": "/game-of-life/static/js/runtime-main.1dafd470.js"
+  }
+]);
